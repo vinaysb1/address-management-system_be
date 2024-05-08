ed6257b1eb5fae5417632c60810e71a808dc1afb
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-05-08 17:56:43 +0530

    middleware added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import addressRoutes from './routes/addressRoutes.js'
 import setupSwagger from './swagger/swagger.js';
 import cors from 'cors'
+import { loggerMiddleware } from './middlewares/loggerMiddleware.js';
 
 const app = express();
 
@@ -10,8 +11,10 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors())
 
-app.use('/address', addressRoutes);
 
+app.use(loggerMiddleware);
+
+app.use('/address', addressRoutes);
 setupSwagger(app);
 
 
